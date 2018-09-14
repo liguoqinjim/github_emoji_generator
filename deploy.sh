b4c37d299dcf10547231decd1817d0e37fbd15ca
liguoqinjim <liguoqinjim23@gmail.com>
2018-09-14 20:40:37 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,9 @@ ls
 echo "add"
 git add -f --ignore-errors --all
 echo "commit"
-git -c user.name='liguoqinjim' -c user.email='liguoqinjim23@gmail.com' commit -m init
+git -c user.name='liguoqinjim' -c user.email='liguoqinjim23@gmail.com' commit -m "deploy by travis"
 git push -f -q https://${GITHUB_TOKEN}@github.com/liguoqinjim/github_emoji.git master
+
 #
 #echo "生成liguoqinjim.com"
 #cd ..
--- download.sh
+++ download.sh
@@ -2,6 +2,7 @@
 
 # github_api
 curl -o emojis.json https://api.github.com/emojis
+cat emojis.json
 
 # unicode_emoji
 curl -o full-emoji-list.html https://unicode.org/emoji/charts/full-emoji-list.html
